5a2f2c35adb9ce23cf1e6afa67bdc91e0ddbc1c1
rlt3 <rlt3@utk.edu>
2022-02-06 21:05:25 -0500

    Adding foundation for find-and-replace. It takes the typical sed input as an expression, e.g. s/find/replace/g. Can find and replace but only for lines that don't have multiple matches. Also missing some features such as searching only in specific lines.

--- find.c
+++ find.c
@@ -11,6 +11,17 @@
  * PROPERTIES
  */
 
+typedef struct replace_properties {
+    int is_all_lines;    /* replace matches on all lines? */
+    int is_single_line;  /* replace only on a single line? */
+    int is_confirm;      /* confirm before each replace? */
+    int is_global;       /* replace multiple matches on each line? */
+    int is_ignore_case;  /* ignore character case when searching? */
+    int start_line;      /* the starting and ending lines of the replacement */
+    int end_line;
+    char *replacement;   /* the string replacing the matches */
+} replace_properties;
+
 typedef struct matchframe {
     /* which frame this holds information for */
     yed_frame *yed_frame;
@@ -37,7 +48,6 @@ static array_t _pattern;
 static regex_t _regex;
 
 static char *_cmd_prompt;
-static unsigned _num_matching;
 
 /**
  * MATCHFRAME
@@ -191,7 +201,7 @@ static inline int find_matchframe_push_match(matchframe *mf,
     return matches[0].rm_eo + 1;
 }
 
-int find_matchframe_search_in_buffer(matchframe *mf) {
+int find_matchframe_search_in_buffer(matchframe *mf, int is_global) {
     /* TODO: execute flags */
     static const int flags = 0;
     /*
@@ -223,7 +233,7 @@ int find_matchframe_search_in_buffer(matchframe *mf) {
         offset = 0;
         while (offset < len) {
             status = regexec(&_regex, line + offset, nmatches, match, flags);
-            if (status != 0)
+            if (status != 0 || !is_global)
                 break;
             offset += find_matchframe_push_match(mf, row, offset, nmatches, match);
         }
@@ -307,9 +317,10 @@ static inline void find_pattern_error(const int status) {
     }
 }
 
-int find_pattern_compile() {
-    /* TODO: compilation flags */
-    static const int flags = 0;
+int find_pattern_compile(int is_ignore_case) {
+    int flags = 0;
+    if (is_ignore_case)
+        flags |= REG_ICASE;
     return regcomp(&_regex, array_data(_pattern), flags);
 }
 
@@ -349,7 +360,6 @@ void find_search_interactive_cancel(matchframe *mf) {
 
     find_pattern_clear();
     find_matchframe_clear(mf);
-    _num_matching = 0;
 }
 
 void find_search_interactive_finish() {
@@ -369,6 +379,7 @@ void find_regex_search(int n_args, char **args) {
     int         key;
     int         status;
     int         row, col;
+    int         num_matches;
 
     if (!ys->active_frame || !ys->active_frame->buffer)
         return;
@@ -403,15 +414,15 @@ void find_regex_search(int n_args, char **args) {
         }
     }
 
-    status = find_pattern_compile();
+    status = find_pattern_compile(0);
     if (status != 0) {
         if (!ys->interactive_command)
             find_pattern_error(status);
         return;
     }
 
-    _num_matching = find_matchframe_search_in_buffer(mf);
-    if (_num_matching == 0) {
+    num_matches = find_matchframe_search_in_buffer(mf, 1);
+    if (num_matches == 0) {
         if (!ys->interactive_command)
             find_pattern_bad();
         /* otherwise, just reset the row, col to original location */
@@ -427,6 +438,188 @@ reset_cursor:
     }
 
     yed_set_cursor_far_within_frame(frame, row, col);
+}
+
+void find_fill_match_buff(char *buff, int size, char *str, regmatch_t match) {
+    int len = match.rm_eo - match.rm_so;
+    if (len == 0) {
+        buff[0] = '\0';
+        return;
+    }
+    if (len > size)
+        len = size;
+    strncpy(buff, str + match.rm_so, len);
+    buff[len] = '\0';
+}
+
+/*
+ * Use regex for parsing a regex
+ */
+int find_parse_replace_expression(matchframe *mf,
+                                  replace_properties *rp,
+                                  char *exp)
+{
+    /*
+     * Matches into 6 groups:
+     * 0) the entire match
+     * 1) beginning line or % for all lines
+     * 2) end line
+     * 3) find expression
+     * 4) replace expression
+     * 5) search modifiers
+     */
+    static const char *pattern = "([0-9]+|%)?,?([0-9]+)?s\\/(.*)\\/(.*)\\/(\\w+)?";
+    static const size_t nmatches = 6;
+    regmatch_t match[nmatches];
+    regex_t regex;
+    char buff[256];
+    int status;
+
+    /* defaults for the find & replace properties */
+    rp->is_all_lines = 0;
+    rp->is_single_line = 0;
+    rp->is_global = 0;
+    rp->is_confirm = 0;
+    rp->is_ignore_case = 0;
+    rp->start_line = -1;
+    rp->end_line = -1;
+    rp->replacement = NULL;
+
+    status = regcomp(&regex, pattern, REG_EXTENDED);
+    if (status != 0) {
+        regerror(status, &regex, buff, 256);
+        yed_cerr("%s", buff);
+        return 1;
+    }
+
+    status = regexec(&regex, exp, nmatches, match, 0);
+    if (status != 0) {
+        yed_cerr("Invalid replace expression!");
+        return 1;
+    }
+
+    /*
+     * If the 1st match is empty then check the 2nd match. If that is empty,
+     * then we're doing a find & replace on the current line, otherwise we're
+     * doing a find and replace on the given line.
+     */
+    find_fill_match_buff(buff, 256, exp, match[1]);
+    if (buff[0] == '\0') {
+        find_fill_match_buff(buff, 256, exp, match[2]);
+        if (buff[0] != '\0')
+            sscanf(buff, "%d", &rp->start_line);
+        else
+            rp->start_line = mf->yed_frame->cursor_line;
+        rp->is_single_line = 1;
+    }
+    /*
+     * If the 1st match is '%' then we are searching all lines. Verify that the
+     * 2nd is empty.
+     */
+    else if (strcmp(buff, "%") == 0) {
+        find_fill_match_buff(buff, 256, exp, match[2]);
+        if (buff[0] != '\0') {
+            yed_cerr("Expression cannot provide both '\%' and ending line number!");
+            return 1;
+        }
+        rp->is_all_lines = 1;
+    }
+    /*
+     * The 1st match must be a number which is the starting line for the
+     * search.  Verify the 2nd match isn't empty. The 2nd match is the ending
+     * line for the search.
+     */
+    else {
+        sscanf(buff, "%d", &rp->start_line);
+        find_fill_match_buff(buff, 256, exp, match[2]);
+        if (buff[0] != '\0') {
+            yed_cerr("Expression provided starting line number but no ending!");
+            return 1;
+        }
+        sscanf(buff, "%d", &rp->end_line);
+    }
+
+    /*
+     * If the 3rd match wasn't provided then we need to use the internally
+     * saved pattern (from find-in-buffer). Otherwise, we replace the internal
+     * pattern with the provided one.
+     */
+    find_fill_match_buff(buff, 256, exp, match[3]);
+    if (buff[0] != '\0')
+        find_pattern_replace(buff);
+
+    /*
+     * If the 4th pattern wasn't provided, then we will replace with nothing,
+     * or remove the matches found from the buffer.
+     */
+    find_fill_match_buff(buff, 256, exp, match[4]);
+    rp->replacement = strdup(buff);
+
+    /*
+     * If the 5th match exists, then we need to match specific search options.
+     * 'g' -> replace every match in the line
+     * 'c' -> confirm the replacement before changing int
+     * 'i' -> ignore case
+     */
+    find_fill_match_buff(buff, 256, exp, match[5]);
+    if (buff[0] != '\0') {
+        if (strchr(buff, 'g') != NULL)
+            rp->is_global = 1;
+        if (strchr(buff, 'c') != NULL)
+            rp->is_confirm = 1;
+        if (strchr(buff, 'i') != NULL)
+            rp->is_ignore_case = 1;
+    }
+
+    return 0;
+}
+
+void find_regex_replace(int n_args, char **args) {
+    replace_properties rp;
+    yed_frame         *frame;
+    yed_buffer        *buffer;
+    matchframe        *mf;
+    match             *m;
+    int                num_matches;
+
+    if (n_args == 0 || n_args > 1) {
+        yed_cerr("Expected 1 argument, received %d", n_args);
+        return;
+    }
+
+    if (!ys->active_frame || !ys->active_frame->buffer)
+        return;
+    frame = ys->active_frame;
+
+    mf = find_matchframe_get_or_create(frame);
+    if (find_parse_replace_expression(mf, &rp, args[0]) != 0)
+        return;
+
+    find_pattern_compile(rp.is_ignore_case);
+    /* TODO: search from start to end line, or specific lines */
+    num_matches = find_matchframe_search_in_buffer(mf, rp.is_global);
+    if (num_matches == 0) {
+        find_pattern_bad();
+        return;
+    }
+
+    buffer = mf->yed_frame->buffer;
+    array_traverse(mf->matches, m) {
+        /*
+         * TODO:
+         * This works for removing the entire match if only one match exists
+         * in the line. Removing and inserting characters into the line affects
+         * the match column offsets.
+         */
+        for (unsigned col = m->start; col < m->end; col++) {
+            yed_delete_from_line(buffer, m->line, m->start + 1);
+        }
+        //for (int i = 0; rp.replacement[i] != '\0'; i++) {
+        //    yed_insert_into_line(buffer, m->line, m->line + i, G(rp.replacement[i]));
+        //}
+    }
+
+    find_matchframe_clear(mf);
 }
 
 void find_cursor_nearest_match(int n_args, char **args, int direction) {
@@ -477,16 +670,11 @@ void find_set_search_prompt(int n_args, char **args) {
     _cmd_prompt = strdup(args[0]);
 }
 
-void find_unload (yed_plugin *self) {
+void find_unload(yed_plugin *self) {
     yed_event_handler h;
     matchframe *mf;
 
-    /* remove our custom search highlighter */
-    h.kind = EVENT_LINE_PRE_DRAW;
-    h.fn   = find_matchframe_highlight_handler;
-    yed_delete_event_handler(h);
-
-    /* re-load the default search highlighter */
+    /* replace the default search highlighter */
     h.kind = EVENT_LINE_PRE_DRAW;
     h.fn   = yed_search_line_handler;
     yed_add_event_handler(h);
@@ -498,6 +686,8 @@ void find_unload (yed_plugin *self) {
     array_free(_pattern);
 
     free(_cmd_prompt);
+
+    regfree(&_regex);
 }
 
 int yed_plugin_boot(yed_plugin *self) {
@@ -517,7 +707,7 @@ int yed_plugin_boot(yed_plugin *self) {
     /* add our custom search highlighter */
     h.kind = EVENT_LINE_PRE_DRAW;
     h.fn   = find_matchframe_highlight_handler;
-    yed_add_event_handler(h);
+    yed_plugin_add_event_handler(self, h);
 
     /*
      * TODO: Event handler for activating frame or loading frame, or otherwise
@@ -526,6 +716,7 @@ int yed_plugin_boot(yed_plugin *self) {
      */
 
     yed_plugin_set_command(self, "find-in-buffer", find_regex_search);
+    yed_plugin_set_command(self, "find-and-replace", find_regex_replace);
     yed_plugin_set_command(self, "find-next-in-buffer", find_cursor_next_match);
     yed_plugin_set_command(self, "find-prev-in-buffer", find_cursor_prev_match);
     yed_plugin_set_command(self, "find-set-search-all-frames", find_set_search_all_frames);
